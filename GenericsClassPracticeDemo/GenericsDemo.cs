5a0f18ece8f412553aa5411df3a2bbbf235306b9
“codewitherrors” <codewitherrors1@gmail.com>
2023-05-09 10:09:14 +0530

    Add : Generic Example

---
+++ GenericsClassPracticeDemo/GenericsDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsClassPracticeDemo
+{
+    //Generic means the general form .
+    //It doesn't specific to a particular dataytype datatype.
+    //We are using <> (Angular Bracket) with type name.
+    // TypeName<T> T is your Type parameter.
+    //Generic allow us to a single class or method that can be used in different datatype.
+    //Advantages is like we can reuse our code.
+    public class GenericsDemo<T>
+    {
+        //It is variable of type T.
+        public T data;
+        //Generic Constructor.
+        public GenericsDemo(T data) 
+        {
+            this.data = data;
+            Console.WriteLine($"Data : {this.data}");
+        }
+
+        public void DemoData(T data)
+        {
+            Console.WriteLine($"Data : {data}");
+        }
+        public void DemoMethod<T>(T data)
+        {
+            Console.WriteLine($"Data : {data}");
+        }
+    }
+}
---
+++ GenericsClassPracticeDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GenericsClassPracticeDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            GenericsDemo<string> genericDemo = new GenericsDemo<string>("Sagar");
+            GenericsDemo<int> generic = new GenericsDemo<int>(9);
+            GenericsDemo<double> gen = new GenericsDemo<double>(9.52);
+            genericDemo.DemoData("Shahu");
+            genericDemo.DemoMethod<string>("Krishna");
+            genericDemo.DemoMethod<int>(25);
+            genericDemo.DemoMethod<double>(25.256);
+            Console.ReadKey();
+
+        } 
+    }
+}
